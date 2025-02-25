2f97a10d85e26818356764cb2effc1430c7173ef
bbrtj <bbrtj.pro@gmail.com>
2025-02-25 18:42:21 +0100

    Remove unneeded escaping of html (handled by Mojo::Template)

--- lib/Bot/Schema/Snippet.pm
+++ lib/Bot/Schema/Snippet.pm
@@ -3,7 +3,6 @@ package Bot::Schema::Snippet;
 use v5.40;
 
 use Data::ULID qw(ulid);
-use Mojo::Util qw(xml_escape);
 
 use parent 'Bot::Schema';
 
@@ -28,10 +27,5 @@ sub prepare_and_save ($self)
 		unless $self->created_at;
 
 	return $self->save;
-}
-
-sub snippet_html ($self)
-{
-	return xml_escape($self->snippet);
 }
 
